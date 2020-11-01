646f6d2780981a39fdc01285439f817a8692d5bf
andras-szesztai <andras@newzoo.com>
2020-11-01 12:59:44 +0100

    rendering chart, axis labels, init only

--- src/components/VerticalBarChart/VerticalBarChart.js
+++ src/components/VerticalBarChart/VerticalBarChart.js
@@ -1,20 +1,25 @@
 import React from "react"
 import styled from "styled-components"
 import uniq from "lodash/uniq"
+import groupBy from "lodash/groupBy"
+import sumBy from "lodash/sumBy"
+import max from "lodash/max"
 import { scaleBand, scaleLinear } from "d3-scale"
 import { stack } from "d3-shape"
 import { axisBottom } from "d3-axis"
+import { select } from "d3-selection"
 import "d3-transition"
 import { useMeasure, usePrevious } from "react-use"
 
 import { QUESTION_TYPES } from "../../constants/filterValues.ts"
+import { CHART_DOMAINS } from "../../constants/chartDomains.ts"
+import { CHART_COLORS } from "../../constants/colors.ts"
+import { COLORS } from "../../constants/colors"
 
 const ChartContainer = styled.div`
   height: 100%;
   width: 100%;
   position: relative;
-
-  border: 1px solid black;
 `
 
 const ChartSvg = styled.svg`
@@ -23,45 +28,98 @@ const ChartSvg = styled.svg`
   height: 100%;
 `
 const margin = {
-  top: 10,
+  top: 16,
   right: 0,
   bottom: 16,
   left: 0,
 }
 
 const VerticalBarChart = ({ data, question }) => {
-  const [divRef, { width }] = useMeasure()
+  const [divRef, { width, height }] = useMeasure()
   const prevWidth = usePrevious(width)
 
   const getXScale = () =>
     scaleBand()
-      .domain(uniq(data.map((d) => d.labelGroup)))
+      .domain(Object.keys(CHART_DOMAINS[question.type]))
       .range([margin.left, width - margin.right])
-      .padding(0.1)
+      .paddingInner(0.3)
+      .paddingOuter(0.2)
 
   const isInitialized = React.useRef(false)
   React.useEffect(() => {
     if (!isInitialized.current && width && data) {
+      isInitialized.current = true
       const xScale = getXScale()
+      const yValues = Object.values(groupBy(data, "labelGroup")).map((d) =>
+        sumBy(d, "value")
+      )
+      const yScale = scaleLinear()
+        .domain([0, max(yValues)])
+        .range([height - margin.bottom, margin.top])
       const xDomain = xScale.domain()
-      let formattedData = []
-      xDomain.forEach((group) => {
-        let labelObj = {}
-        const filtered = data.filter((d) => d.labelGroup === group)
-        filtered.forEach((d) => {
-          labelObj = { ...labelObj, [d.label]: d.value }
+      let combinedStackedData = []
+      xDomain.forEach((val) => {
+        const stackGen = stack().keys(CHART_DOMAINS[question.type][val])
+        const groupFiltered = data.filter((d) => d.labelGroup === val)
+        let dataObj = { group: val }
+        groupFiltered.forEach((d) => {
+          dataObj = { ...dataObj, [d.label]: d.value }
         })
-        formattedData = [...formattedData, { group: group, ...labelObj }]
+        combinedStackedData = [...combinedStackedData, stackGen([dataObj])]
       })
-      const stackGen = stack().keys(QUESTION_TYPES[question.type])
-      const stackedData = stackGen(formattedData)
-      console.log("VerticalBarChart -> stackedData", stackedData)
+      select(chartAreaRef.current)
+        .selectAll("g")
+        .data(combinedStackedData, (d) => d.labelGroup)
+        .enter()
+        .append("g")
+      select(chartAreaRef.current)
+        .selectAll("g")
+        .selectAll("rect")
+        .data(
+          (d) => d,
+          (d) => d.label
+        )
+        .join((enter) =>
+          enter
+            .append("rect")
+            .each((d) => console.log(d[0], yScale(d[0][0]), yScale(d[0][1])))
+            .attr("fill", (d) => CHART_COLORS[question.type][d.key])
+            .attr("x", (d) => xScale(d[0].data.group))
+            .attr("width", xScale.bandwidth())
+            .attr("y", (d) => yScale(d[0][1]))
+            .attr("height", 10)
+            .attr("height", (d) => yScale(d[0][0]) - yScale(d[0][1]))
+            .call((enter) => enter)
+        )
+        select(chartAreaRef.current)
+        .selectAll("text")
+        .data(xDomain)
+        .enter()
+        .append("text")
+        .attr("fill", COLORS.primaryGray)
+        .attr("x", d => xScale(d) + xScale.bandwidth()/2)
+        .attr("y", height)
+        .attr("text-anchor", "middle")
+        .text(d => d)
     }
-  }, [width, data])
+  })
 
+  const chartAreaRef = React.useRef()
   return (
     <ChartContainer ref={divRef}>
-      <ChartSvg />
+      <ChartSvg>
+        <g
+          ref={chartAreaRef}
+        />
+        <line
+          x1={0}
+          x2={width}
+          y1={height-margin.bottom}
+          y2={height-margin.bottom}
+          strokeWidth={1}
+          stroke={COLORS.primaryGray}
+        />
+      </ChartSvg>
     </ChartContainer>
   )
 }
--- src/constants/chartDomains.ts
+++ src/constants/chartDomains.ts
@@ -1,26 +1,22 @@
 export const CHART_DOMAINS: { [key: string]: { [key: string]: string[] } } = {
   accuracy: {
-    "No answer": ["No answer"],
     Inaccurate: ["Very inaccurate", "Somewhat inaccurate"],
-    Accurate: ["Very accurate", "Somewhat accurate"],
+    Accurate: ["Somewhat accurate", "Very accurate"],
   },
   satisfaction: {
-    "No answer": ["No answer"],
     Dissatisfied: ["Very dissatisfied", "Somewhat dissatisfied"],
     Neither: ["Neither satisfied nor dissatisfied"],
-    Satisfied: ["Very satisfied", "Somewhat satisfied"],
+    Satisfied: ["Somewhat satisfied", "Very satisfied"],
   },
   agree: {
-    "No answer": ["No answer"],
     Disagree: ["Very disagree", "Somewhat disagree"],
-    Agree: ["Very agree", "Somewhat agree"],
+    Agree: [ "Somewhat agree", "Very agree"],
   },
   selection: {
     Selected: ["Selected"],
     "Not selected": ["Not selected"],
   },
   language: {
-    "No answer": ["No answer"],
     None: ["None"],
     Basic: ["Basic"],
     Intermediate: ["Intermediate"],
@@ -29,11 +25,9 @@ export const CHART_DOMAINS: { [key: string]: { [key: string]: string[] } } = {
   sex: {
     Male: ["Male"],
     Female: ["Female"],
-    "No answer": ["No answer"],
   },
   boolean: {
     Yes: ["Yes"],
     No: ["No"],
-    "No answer": ["No answer"],
   },
 }
